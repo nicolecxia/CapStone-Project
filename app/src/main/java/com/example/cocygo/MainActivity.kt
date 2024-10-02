82734644667235645fdb2dacf3f38eb710f66baf
Arezou <Arezoughorbaniii@gmail.com>
2024-10-02 18:53:20 -0400

    String file updated and animation added to main Activity

--- app/src/main/java/com/example/cocygo/MainActivity.kt
+++ app/src/main/java/com/example/cocygo/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.cocygo
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import com.example.cocygo.intro.SlidePageMenu
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Show the Hi data name
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val fadeInAnimation = AnimationUtils.loadAnimation(this, R.animator.fade_in)
+
+        // Show the Hi data name
+        val introTV = findViewById<TextView>(R.id.text_welcome)
+      //  name.text = "Hi $dataName,"
+        introTV.startAnimation(fadeInAnimation)
+        // Delay for 5 seconds before navigating to SlidePageMenu
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, SlidePageMenu::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000) // 5000 milliseconds = 5 seconds
     }
 }
