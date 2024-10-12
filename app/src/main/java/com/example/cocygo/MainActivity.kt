fc738133ac16a51a3d640e5ff8ef0451e363c2ca
Arezou <Arezoughorbaniii@gmail.com>
2024-10-12 03:09:50 -0400

    MainActivity.kt updated for fragment

--- app/src/main/java/com/example/cocygo/MainActivity.kt
+++ app/src/main/java/com/example/cocygo/MainActivity.kt
@@ -7,8 +7,10 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.view.animation.AnimationUtils
-import com.example.cocygo.intro.SlidePageMenu
+import android.widget.ImageView
+import com.example.cocygo.intro.SlidePageMenuFragment
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("ResourceType")
@@ -16,17 +18,32 @@ class MainActivity : AppCompatActivity() {
         //Show the Hi data name
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val fadeInAnimation = AnimationUtils.loadAnimation(this, R.animator.fade_in)
-
-        // Show the Hi data name
+//        val fadeInAnimation = AnimationUtils.loadAnimation(this, R.animator.fade_in)
+//
+//        // Show the Hi data name
         val introTV = findViewById<TextView>(R.id.text_welcome)
-      //  name.text = "Hi $dataName,"
-        introTV.startAnimation(fadeInAnimation)
+        val introIV = findViewById<ImageView>(R.id.image_welcome)
+//      //  name.text = "Hi $dataName,"
+//        introTV.startAnimation(fadeInAnimation)
         // Delay for 5 seconds before navigating to SlidePageMenu
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(this, SlidePageMenu::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 5000) // 5000 milliseconds = 5 seconds
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, SlidePageMenu::class.java)
-            startActivity(intent)
-            finish()
+            supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.fragment_container,
+                    SlidePageMenuFragment()
+                ) // Use the correct ID of your fragment container
+                .addToBackStack(null) // Optional: Add to back stack if you want to navigate back
+                .commit()
+        }, 5000) // 5000 milliseconds = 5 seconds
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            introTV.visibility = View.GONE
+            introIV.visibility = View.GONE
         }, 5000) // 5000 milliseconds = 5 seconds
     }
 }
