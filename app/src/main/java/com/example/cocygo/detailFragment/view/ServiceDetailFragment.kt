279ef20e1a2b64ad6a25c586c35ffbce271478eb
Arezou <Arezoughorbaniii@gmail.com>
2024-10-19 20:03:26 -0400

    some change in ui

--- app/src/main/java/com/example/cocygo/detailFragment/view/ServiceDetailFragment.kt
+++ app/src/main/java/com/example/cocygo/detailFragment/view/ServiceDetailFragment.kt
@@ -1,18 +1,23 @@
 package com.example.cocygo.detailFragment.view
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Base64
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import com.example.capstonefragment.viewModel.ServiceDetailViewModel
 import com.example.capstonefragment.viewModel.ServicesListViewModel
 import com.example.cocygo.databinding.FragmentServiceDetailBinding
 
 
-class ServiceDetailFragment : Fragment() {
+class ServiceDetailFragment(image: String?, name: String?, tittle: String?) : Fragment() {
     private var binding: FragmentServiceDetailBinding? = null
+    var name = name
+    var image = image
+    var tittle = tittle
 
     //ViewModel
     private lateinit var servicesListViewModel: ServicesListViewModel
@@ -34,12 +39,19 @@ class ServiceDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        servicesListViewModel = ViewModelProvider(requireActivity())[ServicesListViewModel::class.java]
+        servicesListViewModel =
+            ViewModelProvider(requireActivity())[ServicesListViewModel::class.java]
+        val bitmap = decodeBase64ToBitmap(image!!.trim())
 
-        servicesListViewModel.serviceLists.observe(requireActivity()){ serviceLists ->
-            binding?.title?.text = serviceLists[0].serviceName
-            binding?.subhead?.text = serviceLists[0].stylist
-            binding?.body?.text = serviceLists[0].description
+        binding?.title?.text = tittle
+        binding?.headerImage?.setImageBitmap(bitmap)
+        binding?.body?.text = name
+        servicesListViewModel.serviceLists.observe(requireActivity()) { serviceLists ->
+//            binding?.title?.text = serviceLists[0].serviceName
+//            binding?.subhead?.text = serviceLists[0].stylist
+//            binding?.body?.text = serviceLists[0].description
+
+
         }
 
     }
@@ -49,14 +61,18 @@ class ServiceDetailFragment : Fragment() {
         binding = null
     }
 
-    companion object {
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ServiceDetailFragment().apply {
-                arguments = Bundle().apply {
-
-                }
-            }
+//    companion object {
+//
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            ServiceDetailFragment(item.image, item.name, item.tittle).apply {
+//                arguments = Bundle().apply {
+//
+//                }
+//            }
+//    }
+    private fun decodeBase64ToBitmap(base64: String): Bitmap? {
+        val decodedBytes = Base64.decode(base64, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
     }
 }
--- app/src/main/java/com/example/cocygo/homeFragment/HomeFragment.kt
+++ app/src/main/java/com/example/cocygo/homeFragment/HomeFragment.kt
@@ -1,13 +1,8 @@
 package com.example.cocygo.homeFragment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,10 +10,7 @@ import com.example.cocygo.R
 import com.example.cocygo.detailFragment.view.ServiceDetailFragment
 import com.example.cocygo.homeFragment.adapter.ItemAdapter
 import com.example.cocygo.homeFragment.adapter.ItemViewModel
-import com.example.cocygo.homeFragment.adapter.Service
 import com.example.cocygo.homeFragment.adapter.ServicesListViewModel
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
@@ -41,7 +33,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             adapter = ItemAdapter(items) { item ->
                 // Handle item click if needed
                // Toast.makeText(requireContext(), "Clicked: ${item.name}", Toast.LENGTH_SHORT).show()
-                val serviceDetailFragment = ServiceDetailFragment()
+                val serviceDetailFragment = ServiceDetailFragment(item.image,item.name,item.tittle)
                 //  addToBackStack(null) allows the user to press the back button to return to FirstFragment.
                 parentFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, serviceDetailFragment)
