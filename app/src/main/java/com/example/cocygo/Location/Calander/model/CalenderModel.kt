3a72e2d129bc3d40e100c6f9179c858f95bb83f8
irinaignatenok <64011679+irinaignatenok@users.noreply.github.com>
2024-11-14 21:05:13 -0500

    List Of services

--- app/src/main/java/com/example/cocygo/Location/Calander/model/CalenderModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cocygo.Location.Calander.model
-
-class CalenderModel ( val date: String,
-                      val time: String,
-                      val cartId: String,
-                      val serviceName: String )
